b534e39f1a73598a6cbbf1900f0e454b618ab42e
Friederike Bock <friederike.bock@cern.ch>
2024-06-04 16:23:51 -0400

    updated files for single photon extraction

--- runSinglePhotonExtraction.sh
+++ runSinglePhotonExtraction.sh
@@ -10,4 +10,9 @@ elif [ $1 == "new" ]; then
   for i in 438 439 440 441 442 443 444 450 445 446 447 448 449 451 452 453 454 ; do
     root -b -x -q -l makeSinglePhotonSpectraFitsFromJanusTree.C\+\(\"TestingORNLLocal/04_11_2024_SinglePhotonTest/Run$i\_list.root\"\,\"ProcessedData/SinglePhotonORNLNew\"\,$i\,0\,0\,1\,0\,\"configs/mappingA16_SinglePhoton.txt\"\,kTRUE\,\"configs/ORNL_RunNumbers.txt\"\)
   done
+elif [ $1 == "FullA18" ]; then
+#   for i in 438 ; do
+  for i in 442 ; do
+    root -b -x -q -l makeSinglePhotonSpectraFitsFromJanusTree.C\+\(\"TestingORNLLocal/05_27_2024_SinglePhoton/Run$i\_list.root\"\,\"ProcessedData/SinglePhotonORNL_May2024\"\,$i\,0\,0\,1\,0\,\"configs/mappingA18_SinglePhoton.txt\"\,kTRUE\,\"configs/ORNL_RunNumbers_May2024.txt\"\)
+  done  
 fi
