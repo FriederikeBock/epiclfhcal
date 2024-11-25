a461b207e66e4eff1f9903d7636d8690ab1dddbe
Friederike Bock <friederike.bock@cern.ch>
2024-11-25 08:30:42 -0500

    updated read-me

--- NewStructure/runCalibration_2024.sh
+++ NewStructure/runCalibration_2024.sh
@@ -44,59 +44,71 @@ if [ $2 == "mergemuons" ]; then
 	hadd -f $dataDirRaw/raw_muonScanG_46V.root $dataDirRaw/raw_508.root $dataDirRaw/raw_510.root $dataDirRaw/raw_511.root $dataDirRaw/raw_525.root
 fi
 
+
+# muon runs different scans in groups with separate pedestal
+muonHVScan_44V='305'
+pedHVScan_44V='303'
+
+if [ $2 == "muoncalib" ] || [ $2 == "muoncalibHV44" ]; then
+	echo "running muon calib for 44V runs"
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedHVScan_44V.root -i $dataDirRaw/raw_$muonHVScan_44V.root -o $dataDirRaw/rawPed_$muonHVScan_44V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
+	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_$muonHVScan_44V.root -o $dataDirOut/rawPedAndMuon_$muonHVScan_44V.root -O ../PlotsCalibMuon_2024/muonHVScan_44V -r ../configs/DataTakingDB_202409_CAEN.csv
+
+fi
+
+muonHVScan_43V='307'
+pedHVScan_43V='306'
+if [ $2 == "muoncalib" ] || [ $2 == "muoncalibHV43" ]; then
+	echo "running muon calib for 43V runs"
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedHVScan_43V.root -i $dataDirRaw/raw_$muonHVScan_43V.root -o $dataDirRaw/rawPed_$muonHVScan_43V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
+	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_$muonHVScan_43V.root -o $dataDirOut/rawPedAndMuon_$muonHVScan_43V.root -O ../PlotsCalibMuon_2024/muonHVScan_43V -r ../configs/DataTakingDB_202409_CAEN.csv
 # 
-# # muon runs different scans in groups with separate pedestal
-# muonHVScan_44V='305'
-# pedHVScan_44V='303'
+fi
+muonHVScan_42V='309'
+pedHVScan_42V='308'
+if [ $2 == "muoncalib" ] || [ $2 == "muoncalibHV42" ]; then
+	echo "running muon calib for 42V runs"
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedHVScan_42V.root -i $dataDirRaw/raw_$muonHVScan_42V.root -o $dataDirRaw/rawPed_$muonHVScan_42V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
+	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_$muonHVScan_42V.root -o $dataDirOut/rawPedAndMuon_$muonHVScan_42V.root -O ../PlotsCalibMuon_2024/muonHVScan_42V -r ../configs/DataTakingDB_202409_CAEN.csv
 # 
-# if [ $2 == "muoncalib" ]; then
-# 	echo "running muon calib for 44V runs"
-# # 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedHVScan_44V.root -i $dataDirRaw/raw_$muonHVScan_44V.root -o $dataDirRaw/rawPed_$muonHVScan_44V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
-# 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_$muonHVScan_44V.root -o $dataDirOut/rawPedAndMuon_$muonHVScan_44V.root -O ../PlotsCalibMuon_2024/muonHVScan_44V -r ../configs/DataTakingDB_202409_CAEN.csv
-# 
-# fi
-# 
-# muonHVScan_43V='307'
-# pedHVScan_43V='306'
-# if [ $2 == "muoncalib" ]; then
-# 	echo "running muon calib for 43V runs"
-# # 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedHVScan_43V.root -i $dataDirRaw/raw_$muonHVScan_43V.root -o $dataDirRaw/rawPed_$muonHVScan_43V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
-# 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_$muonHVScan_43V.root -o $dataDirOut/rawPedAndMuon_$muonHVScan_43V.root -O ../PlotsCalibMuon_2024/muonHVScan_43V -r ../configs/DataTakingDB_202409_CAEN.csv
-# # 
-# fi
-# muonHVScan_42V='309'
-# pedHVScan_42V='308'
-# if [ $2 == "muoncalib" ]; then
-# 	echo "running muon calib for 42V runs"
-# # 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedHVScan_42V.root -i $dataDirRaw/raw_$muonHVScan_42V.root -o $dataDirRaw/rawPed_$muonHVScan_42V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
-# 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_$muonHVScan_42V.root -o $dataDirOut/rawPedAndMuon_$muonHVScan_42V.root -O ../PlotsCalibMuon_2024/muonHVScan_42V -r ../configs/DataTakingDB_202409_CAEN.csv
-# # 
-# fi
-# 
-# muonHVScan_41V='312'
-# pedHVScan_41V='311'
-# if [ $2 == "muoncalib" ]; then
-# 	echo "running muon calib for 41V runs"
-# # 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedHVScan_41V.root -i $dataDirRaw/raw_$muonHVScan_41V.root -o $dataDirRaw/rawPed_$muonHVScan_41V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
-# 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_$muonHVScan_41V.root -o $dataDirOut/rawPedAndMuon_$muonHVScan_41V.root -O ../PlotsCalibMuon_2024/muonHVScan_41V -r ../configs/DataTakingDB_202409_CAEN.csv
-# # 
-# fi
-# muonHVScan_40V='316'
-# pedHVScan_40V='315'
-# if [ $2 == "muoncalib" ]; then
-# 	echo "running muon calib for 40V runs"
-# # 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedHVScan_40V.root -i $dataDirRaw/raw_$muonHVScan_40V.root -o $dataDirRaw/rawPed_$muonHVScan_40V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
-# 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_$muonHVScan_40V.root -o $dataDirOut/rawPedAndMuon_$muonHVScan_40V.root -O ../PlotsCalibMuon_2024/muonHVScan_40V -r ../configs/DataTakingDB_202409_CAEN.csv
-# # 
-# fi
+fi
+
+muonHVScan_41V='312'
+pedHVScan_41V='311'
+if [ $2 == "muoncalib" ] || [ $2 == "muoncalibHV41" ]; then
+	echo "running muon calib for 41V runs"
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedHVScan_41V.root -i $dataDirRaw/raw_$muonHVScan_41V.root -o $dataDirRaw/rawPed_$muonHVScan_41V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
+	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_$muonHVScan_41V.root -o $dataDirOut/rawPedAndMuon_$muonHVScan_41V.root -O ../PlotsCalibMuon_2024/muonHVScan_41V -r ../configs/DataTakingDB_202409_CAEN.csv
+fi
+
+muonHVScan_40V='316'
+pedHVScan_40V='315'
+if [ $2 == "muoncalib" ] || [ $2 == "muoncalibHV40" ]; then
+	echo "running muon calib for 40V runs"
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedHVScan_40V.root -i $dataDirRaw/raw_$muonHVScan_40V.root -o $dataDirRaw/rawPed_$muonHVScan_40V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
+	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_$muonHVScan_40V.root -o $dataDirOut/rawPedAndMuon_$muonHVScan_40V.root -O ../PlotsCalibMuon_2024/muonHVScan_40V -r ../configs/DataTakingDB_202409_CAEN.csv
+fi
+
 muonScanA_45V='244 250 282 283'
 pedScanA_45V='271 277'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibA" ]; then
 	echo "running muon calib for 45V runs, campaing A1"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_271.root -i $dataDirRaw/raw_muonScanA1_45V.root -o $dataDirRaw/rawPed_muonScanA1_45V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_271.root -i $dataDirRaw/raw_muonScanA1_45V.root -o $dataDirRaw/rawPed_muonScanA1_45V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanA1_45V.root -o $dataDirOut/rawPedAndMuon_muonScanA1_45V.root -O ../PlotsCalibMuon_2024/muonScanA1_45V -r ../configs/DataTakingDB_202409_CAEN.csv
 	echo "running muon calib for 45V runs, campaing A2"
-	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_277.root -i $dataDirRaw/raw_muonScanA2_45V.root -o $dataDirRaw/rawPed_muonScanA2_45V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_277.root -i $dataDirRaw/raw_muonScanA2_45V.root -o $dataDirRaw/rawPed_muonScanA2_45V.root -r ../configs/DataTakingDB_202409_CAEN.csv
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanA2_45V.root -o $dataDirOut/rawPedAndMuon_muonScanA2_45V.root -O ../PlotsCalibMuon_2024/muonScanA2_45V -r ../configs/DataTakingDB_202409_CAEN.csv
 	
 fi
@@ -105,21 +117,27 @@ muonScanD1_45V='412 417'
 pedScanD1_45V='420'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibD" ]; then
 	echo "running muon calib for 45V runs, campaing D1"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanD1_45V.root -i $dataDirRaw/raw_muonScanD1_45V.root -o $dataDirRaw/rawPed_muonScanD1_45V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanD1_45V.root -i $dataDirRaw/raw_muonScanD1_45V.root -o $dataDirRaw/rawPed_muonScanD1_45V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanD1_45V.root -o $dataDirOut/rawPedAndMuon_muonScanD1_45V.root -O ../PlotsCalibMuon_2024/muonScanD1_45V -r ../configs/DataTakingDB_202409_CAEN.csv
 fi
 muonScanD2_45V='460 456 457'
 pedScanD2_45V='454'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibD" ]; then
 	echo "running muon calib for 45V runs, campaing D2"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanD2_45V.root -i $dataDirRaw/raw_muonScanD2_45V.root -o $dataDirRaw/rawPed_muonScanD2_45V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanD2_45V.root -i $dataDirRaw/raw_muonScanD2_45V.root -o $dataDirRaw/rawPed_muonScanD2_45V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanD2_45V.root -o $dataDirOut/rawPedAndMuon_muonScanD2_45V.root -O ../PlotsCalibMuon_2024/muonScanD2_45V -r ../configs/DataTakingDB_202409_CAEN.csv
 fi
 muonScanH1_45V='526 527'
 pedScanH1_45V='528'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibH" ]; then
 	echo "running muon calib for 45V runs, campaing H1"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanH1_45V.root -i $dataDirRaw/raw_muonScanH1_45V.root -o $dataDirRaw/rawPed_muonScanH1_45V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanH1_45V.root -i $dataDirRaw/raw_muonScanH1_45V.root -o $dataDirRaw/rawPed_muonScanH1_45V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanH1_45V.root -o $dataDirOut/rawPedAndMuon_muonScanH1_45V.root -O ../PlotsCalibMuon_2024/muonScanH1_45V -r ../configs/DataTakingDB_202409_CAEN.csv
 fi
 
@@ -128,7 +146,9 @@ muonScanH2_45V='554 559'
 pedScanH2_45V='552'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibH" ]; then
 	echo "running muon calib for 45V runs, campaing H2"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanH2_45V.root -i $dataDirRaw/raw_muonScanH2_45V.root -o $dataDirRaw/rawPed_muonScanH2_45V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanH2_45V.root -i $dataDirRaw/raw_muonScanH2_45V.root -o $dataDirRaw/rawPed_muonScanH2_45V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanH2_45V.root -o $dataDirOut/rawPedAndMuon_muonScanH2_45V.root -O ../PlotsCalibMuon_2024/muonScanH2_45V -r ../configs/DataTakingDB_202409_CAEN.csv
 fi
 
@@ -137,7 +157,9 @@ muonScanB1_42V='331 322'
 pedScanB1_42V='332'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibB" ]; then
 	echo "running muon calib for 42V runs, campaing B1"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanB1_42V.root -i $dataDirRaw/raw_muonScanB1_42V.root -o $dataDirRaw/rawPed_muonScanB1_42V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanB1_42V.root -i $dataDirRaw/raw_muonScanB1_42V.root -o $dataDirRaw/rawPed_muonScanB1_42V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanB1_42V.root -o $dataDirOut/rawPedAndMuon_muonScanB1_42V.root -O ../PlotsCalibMuon_2024/muonScanB1_42V -r ../configs/DataTakingDB_202409_CAEN.csv
 fi
 
@@ -145,7 +167,9 @@ muonScanB2_42V='370 371 374'
 pedScanB2_42V='369'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibB" ]; then
 	echo "running muon calib for 42V runs, campaing B2"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanB2_42V.root -i $dataDirRaw/raw_muonScanB2_42V.root -o $dataDirRaw/rawPed_muonScanB2_42V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanB2_42V.root -i $dataDirRaw/raw_muonScanB2_42V.root -o $dataDirRaw/rawPed_muonScanB2_42V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanB2_42V.root -o $dataDirOut/rawPedAndMuon_muonScanB2_42V.root -O ../PlotsCalibMuon_2024/muonScanB2_42V -r ../configs/DataTakingDB_202409_CAEN.csv
 fi
 
@@ -153,42 +177,54 @@ muonScanC1_43_5V='376 375'
 pedScanC1_43_5V='377'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibC" ]; then
 	echo "running muon calib for 43.5V runs, campaing C1"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanC1_43_5V.root -i $dataDirRaw/raw_muonScanC1_43_5V.root -o $dataDirRaw/rawPed_muonScanC1_43_5V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanC1_43_5V.root -i $dataDirRaw/raw_muonScanC1_43_5V.root -o $dataDirRaw/rawPed_muonScanC1_43_5V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanC1_43_5V.root -o $dataDirOut/rawPedAndMuon_muonScanC1_43_5V.root -O ../PlotsCalibMuon_2024/muonScanC1_43_5V -r ../configs/DataTakingDB_202409_CAEN.csv
 fi
 muonScanC2_43_5V='405 510 408'
 pedScanC2_43_5V='404'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibC" ]; then
 	echo "running muon calib for 43.5V runs, campaing C2"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanC2_43_5V.root -i $dataDirRaw/raw_muonScanC2_43_5V.root -o $dataDirRaw/rawPed_muonScanC2_43_5V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanC2_43_5V.root -i $dataDirRaw/raw_muonScanC2_43_5V.root -o $dataDirRaw/rawPed_muonScanC2_43_5V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanC2_43_5V.root -o $dataDirOut/rawPedAndMuon_muonScanC2_43_5V.root -O ../PlotsCalibMuon_2024/muonScanC2_43_5V -r ../configs/DataTakingDB_202409_CAEN.csv
 fi
 muonScanE1_40V='463 464'
 pedScanE1_40V='465'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibE" ]; then
 	echo "running muon calib for 40V runs, campaing E1"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanE1_40V.root -i $dataDirRaw/raw_muonScanE1_40V.root -o $dataDirRaw/rawPed_muonScanE1_40V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanE1_40V.root -i $dataDirRaw/raw_muonScanE1_40V.root -o $dataDirRaw/rawPed_muonScanE1_40V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanE1_40V.root -o $dataDirOut/rawPedAndMuon_muonScanE1_40V.root -O ../PlotsCalibMuon_2024/muonScanE1_40V -r ../configs/DataTakingDB_202409_CAEN.csv
 fi
 muonScanE2_40V='481 478'
 pedScanE2_40V='476'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibE" ]; then
 	echo "running muon calib for 40V runs, campaing E2"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanE2_40V.root -i $dataDirRaw/raw_muonScanE2_40V.root -o $dataDirRaw/rawPed_muonScanE2_40V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanE2_40V.root -i $dataDirRaw/raw_muonScanE2_40V.root -o $dataDirRaw/rawPed_muonScanE2_40V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanE2_40V.root -o $dataDirOut/rawPedAndMuon_muonScanE2_40V.root -O ../PlotsCalibMuon_2024/muonScanE2_40V -r ../configs/DataTakingDB_202409_CAEN.csv
 fi
 muonScanF1_41V='486 489'
 pedScanF1_41V='492'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibF" ]; then
 	echo "running muon calib for 41V runs, campaing F1"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanF1_41V.root -i $dataDirRaw/raw_muonScanF1_41V.root -o $dataDirRaw/rawPed_muonScanF1_41V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanF1_41V.root -i $dataDirRaw/raw_muonScanF1_41V.root -o $dataDirRaw/rawPed_muonScanF1_41V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanF1_41V.root -o $dataDirOut/rawPedAndMuon_muonScanF1_41V.root -O ../PlotsCalibMuon_2024/muonScanF1_41V -r ../configs/DataTakingDB_202409_CAEN.csv
 fi
 muonScanF2_41V='507 506'
 pedScanF2_41V='505'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibF" ]; then
 	echo "running muon calib for 41V runs, campaing F2"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanF2_41V.root -i $dataDirRaw/raw_muonScanF2_41V.root -o $dataDirRaw/rawPed_muonScanF2_41V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if [ $3 == "transfer" ]; then 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanF2_41V.root -i $dataDirRaw/raw_muonScanF2_41V.root -o $dataDirRaw/rawPed_muonScanF2_41V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanF2_41V.root -o $dataDirOut/rawPedAndMuon_muonScanF2_41V.root -O ../PlotsCalibMuon_2024/muonScanF2_41V -r ../configs/DataTakingDB_202409_CAEN.csv
 fi
 # 
@@ -196,7 +232,9 @@ muonScanG_46V='508 510 511 525'
 pedScanG_46V='521'
 if [ $2 == "muoncalibAll" ] || [ $2 == "muoncalibG" ]; then
 	echo "running muon calib for 46V runs, campaing G"
-# 	./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanG_46V.root -i $dataDirRaw/raw_muonScanG_46V.root -o $dataDirRaw/rawPed_muonScanG_46V.root -O ../PlotsCalibMuon_2024/Run$runNr -r ../configs/DataTakingDB_202409_CAEN.csv
+	if 
+		./Analyse -d 1 -f -P $dataDirOut/PedestalCalib_$pedScanG_46V.root -i $dataDirRaw/raw_muonScanG_46V.root -o $dataDirRaw/rawPed_muonScanG_46V.root -r ../configs/DataTakingDB_202409_CAEN.csv
+	fi
 	./Analyse -f -d 1 -s -i $dataDirRaw/rawPed_muonScanG_46V.root -o $dataDirOut/rawPedAndMuon_muonScanG_46V.root -O ../PlotsCalibMuon_2024/muonScanG_46V -r ../configs/DataTakingDB_202409_CAEN.csv
 fi
 
--- README.md
+++ README.md
@@ -1,17 +1,104 @@
 # epiclfhcal
 
 This software repo is meant to serve as basis for the test beam root based analysis of the LFHCal test beams and test beam setups.
-The repo contains the initial code used in 2023 under OldStructure & the newer version of the code under NewStructure
+The repo contains the initial code used in 2023 under OldStructure & the newer version of the code under NewStructure <br>
 
+For convenience a linker script is provided, which can be used to recreate the software structure with soft links. Execute it in the directory where you would like to work after having added your user name and path to the software repo
+
+```console
+bash $PATHTOSCRIPT/prepareAnalysisDirectory.sh $USERNAME [new/old]
+```
 
 
 ## OldStructure
 
+This part of the software was initially used for the analysis of the test beam from 2023. The main root macro for the test beam analysis can is
+
+```c+
+OldStructure/makeSimplePlotsFromJanusTree.C
+```
+
+How to start it can be found in ```runJanusExtraction.sh```, which serves as primary steering script for the data analysis. It also contains several examples on how to compare the output of different runs included in: <br>
+
+```c+
+OldStructure/CompareDifferentRuns.C
+```
+
+
+With this code also the single photon extraction can be done, this is steered by ```runSinglePhotonExtraction.sh``` for which the main root macro is 
+```c+
+OldStructure/makeSinglePhotonSpectraFitsFromJanusTree.C
+```
+The comparisons for the single photon spectra are steered by ```runCompareRunsForSinglePhotons.sh``` which have as main root macro:
+
+```c+
+OldStructure/CompareDifferentRunsForSinglePhotonSpectra.C.C
+```
 
 ## NewStructure
+This part of hte software has been developed by Vincent Andrieux (@vandrie) primarily for the 2024 test beam analysis, it is being adapted to be usable also for the 2023 data.
+
+### Running the analysis through scripts
+
+#### September 2023 data
+
+The data taken in September 2023 at the SPS-H4 beam line, was taken without absorber plates presents. The logbook can be found [here](https://docs.google.com/spreadsheets/d/1GBmztS66Cagwr1mpXuaDmIfSKAuhBm1gmhhpk7qSgbg/edit?usp=sharing), if you don't have access please ask Friederike for it. <br>
+The raw data for this campaign was unfortunately partially lost and thus a separate routine had to be introduced reading the data from the initally converted root files. The conversion can be done with:
+
+```console
+bash convertData_Sept2023.sh $USERNAME [singe/all]
+```
+
+As for the ```prepareAnalysisDirectory.sh```, please add your username and the path to the data. The script is under construction and even under all might only have few runs commented in, feel free to uncomment the rest.<br>
+
+The further analysis of this data is still under development and so far only the pedestal extraction is fully working as second step. The corresponding script can be run as follows:
+
+```console
+bash runCalibration_Sept2023.sh $USERNAME pedestal
+```
+Same principle applies as for the converter script. As we further develop the code more functionality will become available.
+
+#### October 2023 data
+The data taken in October 2023 at the PS-T09 beam line, was taken with only 14 layers, parasitic to the FOCal-E with various number of thin tungsten plates infront of the mini-LFHCal module. The logbook can be found [here](https://docs.google.com/spreadsheets/d/1oRI3KlPyHouo5P4J70wLrGlaFaeNuLCGMCwyNxyNMf4/edit?usp=sharing), if you don't have access please ask Friederike for it. <br>
+This data was taken with a previous version of the Janus software, as such the reading works a little differently. So far only the data converter has been tested and adapted 
+
+```console
+bash convertData_Oct2023.sh $USERNAME [singe/all]
+```
+As for the ```prepareAnalysisDirectory.sh``, please add your username and the path to the data. The script is under construction and even under all might only have few runs commented in, feel free to uncomment the rest.<br>
+Attention for a few runs (201,202,203,204,205), the converter will fail as a few channels were accidentally masked breaking the conversion. This data will have to be discarded. As we further develop the code more functionality will become available.
+
+#### 2024 data
+The data taken in August/September 2024 at the PS-T09 beam line is the first data available for a full module. The logbook can be found [here](https://docs.google.com/spreadsheets/d/1XaiSmG4jBaBOyjbjdiNuSeehjeZC03_2A7Ccoq0nIbI/edit?usp=sharing), if you don't have access please ask Friederike for it. A summary of the CAEN data taking campaign can be found on our [wiki](https://wiki.bnl.gov/EPIC/index.php?title=LFHCal_Fall_2024_Test_Beam).<br> 
+The data conversion can be done using the script
+
+```console
+bash convertData_2024.sh $USERNAME [single/all]
+```
+As for the ```prepareAnalysisDirectory.sh```, please add your username and the path to the data.  This script so far contains all pedestal and muon runs under the flag ```all```. Further runs will be added in the future. <br>
+The analysis of the 2024 muon runs is furthest developed. The pedestal extraction can be starting with the following command for all pedestal runs
+```
+bash runCalibration_2024.sh $USERNAME pedestal
+```
+As for the ```prepareAnalysisDirectory.sh```, please add your username and the path to the data, as well as the calibration output directory. The further steps in the analysis , like merging of the raw data for different muon runs can be started with the argument ```mergemuons``` instead of ```pedestal```. In the script all combinations of muon runs and pedestal runs are defined with corresponding variables, i.e. 
+```console
+muonHVScan_44V='305'
+pedHVScan_44V='303'
+```
+The corresponding analysis of the muon calib extraction can be started with 
+```
+bash runCalibration_2024.sh $USERNAME muoncalibHV44 transfer
+```
+the last argument only needs to be used in case a new pedestal calibration has been created and needs to be transferred to the corresponding muon run file. Otherwise the following command is sufficient.
+```
+bash runCalibration_2024.sh $USERNAME muoncalibHV44
+```
+The muon calibration can be started for all muon runs ```muoncalib``` or for specific parts of the campaign ```muoncalib[HV44|HV43|HV42|HV41|HV40|A|B|C|D|E|F|G|H]```. Please be careful if you start all runs, this will take a while and will produce quite some control plots.
+
+### Individual calls of the analysis steps
 Here an example of running the code from ASCII input to calibrated ROOT output format is provided:
 
-0/Compile the code with 
+0. Compile the code with 
 ```console
 make Analyse
 ```
@@ -21,7 +108,12 @@ if you changed something in the base classes majorly first
   make Analyse
 ```
 
-1/Convert ASCII to root (step to be done for every single ASCII file):
+0. As always please run the helper function first to make yourself familar with the options
+```console
+ ./Analyse -h
+```
+
+1. Convert ASCII to root (step to be done for every single ASCII file):
 ```console
 ./Analyse -c PATH_INPUT_FILE/RunXXX_list.txt -o PATH_OUTPUT_FILE/WhateverName.root -m ../configs/mappingFile_202409_CAEN.txt -r ../configs/DataTakingDB_202409_CAEN.csv
 ```
@@ -33,7 +125,7 @@ if you changed something in the base classes majorly first
 ./Analyse -c Run379_list.txt -o RawElectron1GeV.root -m ../configs/mappingFile_202409_CAEN.txt -r ../configs/DataTakingDB_202409_CAEN.csv
 ```
 
-2/Extract pedestal value (currently simple gaussian fit):
+2. Extract pedestal value (currently simple gaussian fit):
 
 ```console
 ./Analyse (-f) -p -i RawPedestal.root -o PedestalCalib.root
@@ -41,20 +133,20 @@ if you changed something in the base classes majorly first
 
 
 
-3/Extract MIP calibration:
-3.1 Conveniently hadd the input file to make sure we should see MIP signal on all tiles (included furthest Jura or Saleve)
+3. Extract MIP calibration:
+  1. Conveniently hadd the input file to make sure we should see MIP signal on all tiles (included furthest Jura or Saleve)
 ```console
 hadd  RawMuonBeamFullAcceptance.root   RawMuonBeamPlus5.root  RawMuonBeamMinus5.root
 ```
 
 
-3.2/ Transfer Calibration object from Pedestal file to MIP file
+  2. Transfer Calibration object from Pedestal file to MIP file
 ```console
 ./Analyse (-f) -P PedestalCalib.root -i RawMuonBeamFullAcceptance.root -o RawMuonBeamFullAcceptancePedCalib.root
 ```
 
 
-3.3/ Extract MIP signal based on Landau fit (request the Pedestal file to shift the ADC distribution to get rid of most of the noise, the tail is still taken into account from the values stored in the calibration object and due to potential deviation in the tail, a decreasing exponential is also added)
+  3. Extract MIP signal based on Landau fit (request the Pedestal file to shift the ADC distribution to get rid of most of the noise, the tail is still taken into account from the values stored in the calibration object and due to potential deviation in the tail, a decreasing exponential is also added)
 
 ```console
 ./Analyse (-f) -s -i RawMuonBeamFullAcceptancePedCalib.root -o RawMuonBeamFullAcceptancePedAndScaleCalib.root
@@ -66,7 +158,7 @@ hadd  RawMuonBeamFullAcceptance.root   RawMuonBeamPlus5.root  RawMuonBeamMinus5.
 
 
 
-4/Apply Calibration to physics data
+4. Apply Calibration to physics data
 ```console
 ./Analyse (-f) -C RawMuonBeamFullAcceptancePedAndScaleCalib.root -i RawElectron1GeV.root -o CalibratedElectron1GeV.root
 ```
